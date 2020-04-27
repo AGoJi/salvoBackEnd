a13f852aaab7a4c349ee50cbf16ef6d630226f7e
Albert Gomicia <a.gomicia.ubiqum@gmail.com>
2020-04-27 13:16:52 +0200

    deleted un used packege

--- src/main/java/com/codeoftheweb/salvo/GamePlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/GamePlayerRepository.java
@@ -1,6 +1,5 @@
 package com.codeoftheweb.salvo;
 
-import org.graalvm.compiler.lir.LIR;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.Date;
